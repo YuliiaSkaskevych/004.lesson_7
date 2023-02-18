33b229a2b1c12b24fb4e3ded0baa25a0e5e7f84b
Yuliia00 <skaskevich2000@gmail.com>
2023-02-18 20:23:30 +0100

    part_2

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let num1 = prompt("Input 1st number");
-let num2 = prompt("Input 2nd number");
-let num3 = prompt("Input 3rd number");
+let num1 = parseFloat(prompt("Input 1st number"));
+let num2 = parseFloat(prompt("Input 2nd number"));
+let num3 = parseFloat(prompt("Input 3rd number"));
 
-num1=Number(num1)
-num2=Number(num2)
-num3=Number(num3)
+alert(`The arithmetic average: ${(num1+num2+num3)/3}`)
